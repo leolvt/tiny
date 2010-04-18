2fc23263f56b47a1eb1d1298818ea3b6b6197c10
vilela <vilela@dcc.ufmg.br>
2010-04-18 16:11:16 -0300

    Removido código desnecessário no arquivo main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,50 +7,6 @@
 
 using namespace tiny;
 
-#if 0
-int main(int argc, char * argv[])
-{
-	Driver driver;
-	Contexto Ctx;
-
-	if (argc > 1)
-	{
-		string file = string(argv[1]);
-		driver.parse_file(file);
-		driver.programa->Interpreta(Ctx);
-
-		return 0;
-	}
-
-	std::cout << "Reading from stdin" << std::endl;
-
-    std::string line;
-    while( std::cout << "input: " &&  std::getline(std::cin, line) &&
-	   !line.empty() )
-    {
-		
-		line = line + " endp";
-		
-		bool result = driver.parse_string(line, "input");
-
-		if (result)
-		{
-			if (driver.programa)
-			{
-				driver.programa->Interpreta(Ctx);
-				std::cout << std::endl;
-			}
-		}
-		else
-		{
-			std::cout << "Erro!!" << std::endl;
-		}
-    }
-
-	return 0;
-}
-#endif
-
 int main(int argc, char *argv[])
 {
     Contexto Ctx;
