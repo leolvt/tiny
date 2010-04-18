e15ca2a670b84e3972ff01f7628c0e2d75bf5fd9
vilela <vilela@dcc.ufmg.br>
2010-04-18 16:08:57 -0300

    * Corrigida a sintaxe do arquivo de teste while.tiny * Adicionados estilos necessários para gerar a documentação (LaTeX) * Renomeado o diretório documentação para doc. * Modificado main.cpp para melhor execução.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+
 #include "driver.h"
 #include "contexto.h"
-using namespace tiny;
-using namespace std;
 
-#if 1
+using namespace tiny;
+
+#if 0
 int main(int argc, char * argv[])
 {
 	Driver driver;
@@ -48,3 +50,61 @@ int main(int argc, char * argv[])
 	return 0;
 }
 #endif
+
+int main(int argc, char *argv[])
+{
+    Contexto Ctx;
+    Driver driver;
+    bool readfile = false;
+
+    for(int ai = 1; ai < argc; ++ai)
+    {
+        if (argv[ai] == std::string ("-p")) {
+            driver.trace_parsing = true;
+        }
+        else if (argv[ai] == std::string ("-s")) {
+            driver.trace_scanning = true;
+        }
+        else
+        {
+            // Lendo de um arquivo
+
+            std::fstream infile(argv[ai]);
+            if (!infile.good())
+            {
+                std::cerr << "Erro ao abrir arquivo: " << argv[ai] << std::endl;
+                return 0;
+            }
+
+            bool result = driver.parse_stream(infile, argv[ai]);
+            if (result)
+            {
+                if (driver.programa) driver.programa->Interpreta(Ctx);
+                else std::cerr << "Erro ao processar o código" << std::endl;
+				std::cout << std::endl;
+            }
+
+            readfile = true;
+        }
+    }
+
+    if (readfile) return 0;
+   
+    std::cout << "Lendo programa da entrada padrão: " << std::endl;
+
+    std::string line;
+    while( std::cout << "entrada: " &&
+           std::getline(std::cin, line) &&
+           !line.empty() )
+    {
+        bool result = driver.parse_string(line, "entrada");
+
+        if (result)
+        {
+            if (driver.programa) driver.programa->Interpreta(Ctx);
+            else std::cerr << "Erro ao processar o código" << std::endl;
+			std::cout << std::endl;
+			delete driver.programa;
+        }
+    }
+}
